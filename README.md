944e8815ecfafa5c8014c96d3a297d1f4f41edfe
nem035 <n.stojanovic035@gmail.com>
2016-03-07 04:25:20 -0500

    Finished initial version.

---
+++ README.md
@@ -0,0 +1,13 @@
+# QR-gen 
+
+QR-gen is a simple command line utility that creates QR-encoded images.
+
+## Usage
+   
+- Create an image from a string.
+
+      qr <stringToEncode> <pathToQRimage>
+
+- Create an image from a file
+  
+      qr <filePath> <pathToQRimage>
---
+++ qr.js
@@ -0,0 +1,66 @@
+'user strict';
+
+const qr = require('qr-image');
+const fs = require('fs');
+
+const USAGE_STRING = 
+        'Encoding text:\n' +
+        '\t qr <text> <pathToImage>\n\n' +
+        'Encoding a file:\n' +
+        '\t qr -f <pathToFile> <pathToImage>\n\n';
+
+const [ nodePath, scriptPath, arg1, arg2, arg3 ] = process.argv;
+
+// create a sorted arguments array
+const args = [arg1, arg2, arg3].filter(a => !!a);
+const isFile = args.indexOf('-f') !== -1;
+const stringArgs = args.filter(a => a != '-f');
+
+if (stringArgs.length > 1) {
+
+    const [str, imagePath] = stringArgs; 
+
+    if (isFile) {
+        fileAccess(str)
+            .then(fileRead)
+            .then(content => {
+                console.log(content.toString());
+                createImage(content.toString(), imagePath);
+            });
+    } else {
+        createImage(str, imagePath);
+    }
+} else {
+    console.log(USAGE_STRING);
+}
+
+function createImage(text, path) {
+    qr.image(text, {
+        type: 'png',
+        size: 20
+    }).pipe(fs.createWriteStream(path));
+}
+
+function fileAccess(path) {
+    return new Promise((resolve, reject) => {
+        fs.access(path, fs.F_OK | fs.R_OK, (error) => {
+            if (!error) {
+                resolve(path);
+            } else {
+                reject(error);
+            }
+        });
+    });
+}
+
+function fileRead(path) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(path, (error, content) => {
+            if (!error) {
+                resolve(content);
+            } else {
+                reject(error);
+            }
+        });
+    });
+}
